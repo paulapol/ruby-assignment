938cb2b029c3bdfc3d1be445f20ca34c4e5615c1
paulapol <paula.pol98@yahoo.ro>
2020-07-28 16:58:27 +0300

    fix some indentation and consistency problems

--- car_wash.rb
+++ car_wash.rb
@@ -6,17 +6,20 @@ class Car_wash
 
 attr_accessor :enter_time, :reservation
 def initialize
-    @reservation=Array.new
+    @reservation = Array.new
     @enter_time = Time.now
 end
 
 START_SCHEDULE = 8
 END_SCHEDULE = 16
-START_SCHEDULE_WEEKEND= 9
-END_SCHEDULE_WEEKEND= 14
+START_SCHEDULE_WEEKEND = 9
+END_SCHEDULE_WEEKEND = 14
+FORTY_THREE_HOURS = 43*60*60
+SEVENTEEN_HOURS = 17*60*60
+SIXTEEN_HOURS = 16*60*60
 
 def schedule(enter_time) 
-    today=enter_time.strftime("%A")
+    today = enter_time.strftime("%A")
     case today
     when "Saturday"
         open_hour = START_SCHEDULE_WEEKEND
@@ -27,56 +30,50 @@ def schedule(enter_time)
         open_hour = START_SCHEDULE
         close_hour = END_SCHEDULE
     end
-
     @today_open = open_hour   
-    @today_close = close_hour
-    
+    @today_close = close_hour   
 end
 
 def can_process_now?(enter_time)
-    if @reservation.empty? || @reservation.length() == 1 || [@reservation[-1].pickup_time, @reservation[-2].pickup_time].any? {|pickup_time| pickup_time <enter_time }
+    if @reservation.empty? || @reservation.length() == 1 || [@reservation[-1].pickup_time, @reservation[-2].pickup_time].any? {|pickup_time| pickup_time <enter_time}
         true
-   else
+    else
         false
-   end
+    end
 end 
 
 def worker_availability(enter_time)
+    pickup_time = process_request(enter_time)
     if @reservation.empty? 
-        return [1, true, process_request(enter_time)]
+        return { worker_id: 1, pickup_time: pickup_time}
     end
     if @reservation.length() == 1
-        return [3 - @reservation[0].worker, true, process_request(enter_time)]
+        return {worker_id: 3 - @reservation[0].worker, pickup_time: pickup_time}
     end
     if @reservation[-2].pickup_time < enter_time
-        return [@reservation[-2].worker, true, process_request(enter_time)]
+        return {worker_id: @reservation[-2].worker, pickup_time: pickup_time}
     end
     if @reservation[-1].pickup_time < enter_time
-        return [@reservation[-1].worker, true, process_request(enter_time)]
+        return {worker_id: @reservation[-1].worker, pickup_time: pickup_time}
     end  
     
-    [@reservation[-2].worker, false, process_request(enter_time)]
+    {worker_id: @reservation[-2].worker, pickup_time: pickup_time}
 end
 
 def pickup_time(enter_time)
-    puts enter_time
     schedule(enter_time)
-    availability=worker_availability(enter_time)
-    #puts availability
     entry=OpenStruct.new
     if can_process_now?(enter_time)
-        entry.enter_time=enter_time
+        entry.enter_time = enter_time
     else
-        entry.enter_time=@reservation[-2].pickup_time
+        entry.enter_time = @reservation[-2].pickup_time
     end
-    
-    availability=worker_availability(entry.enter_time)
-    entry.worker=availability[0]
-    entry.pickup_time=Time.at(availability[2])
+    availability = worker_availability(entry.enter_time)
+    entry.worker = availability[:worker_id]
+    entry.pickup_time = Time.at(availability[:pickup_time])
     @reservation.push(entry)
-    print entry
-    #print @reservation
-    return entry.pickup_time.strftime("%A %d-%m-%Y %H:%M")   
+
+    entry.pickup_time.strftime("%A %d-%m-%Y %H:%M")   
 end
 
 def process_request(process_time)
@@ -84,13 +81,13 @@ def process_request(process_time)
     if @today_close >= process_time.hour 
         out_time = process_time 
     elsif @enter_time.saturday?
-        out_time = process_time+(43*60*60) 
+        out_time = process_time + FORTY_THREE_HOURS 
     elsif @enter_time.friday?
-        out_time =process_time+ (17*60*60) 
+        out_time = process_time + SEVENTEEN_HOURS 
     else
-        out_time = process_time +(16*60*60)         
+        out_time = process_time + SIXTEEN_HOURS         
     end
-    return out_time
+    out_time
 end
 
 end
